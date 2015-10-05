8805b3b95f56231e5c11753e115b37338ec03c15
ermontross <emma.montross@colorado.edu>
2015-10-05 11:09:49 -0600

    added README and completed Node class

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -1,5 +1,6 @@
 #Emma Montross
-#Program Assignment 2
+#Program Assignment 5
+#value iteration 
 
 import sys
 import heapq
@@ -21,11 +22,34 @@ def calcDistance(x1,y1,x2,y2,typ):
 	return 10 + int(x1 != x2 and y1 != y2) * 4 + typ * 10
 
 class Node:
-	def __init__(self,x,y,typ):
+	def __init__(self,x,y,value):
 		self.x = x
 		self.y = y
-		self.utility = 0
-		self.typ = typ
+		#determine utility and reward based on value of sqaure in world matrix
+		if(value == 0):
+			#open square
+			self.reward = 0
+			self.utility = 0
+		elif(value == 1):
+			#mountain
+			self.reward = -1
+			self.utility = -1
+		elif(value == 2):
+			#wall
+			self.reward = None
+			self.utility = None
+		elif(value == 3):
+			#snake
+			self.reward = -2
+			self.utility = -2
+		elif(value == 4):
+			#barn
+			self.reward = 1
+			self.utility = 1
+		elif(value == 50):
+			#goal square
+			self.reward = 50
+			self.utility = 50
 		
 class Value:
 	def __init__(self,world):
@@ -33,5 +57,5 @@ class Value:
 
 
 filename = sys.argv[1]
-h = int(sys.argv[2])
+e = float(sys.argv[2]) #epsilon 
 world = getWorld(filename)
