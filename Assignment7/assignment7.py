c8a1c68719eb276e9baf35a17fc4dec391679afb
ermontross <emma.montross@colorado.edu>
2015-11-02 11:17:17 -0700

    generating samples works

--- Assignment7/assignment7.py
+++ Assignment7/assignment7.py
@@ -4,15 +4,18 @@
 
 import argparse
 
+#read the text file of numbers into a list
 def read_file(filename):
 	infile = open(filename,"r")
 	numbers = []
 	line = infile.readline()
 	while line != "":
-		numbers.append(int(line))
+		numbers.append(float(line))
 		line = infile.readline()
+	print numbers
 	return numbers
 
+#generate the bayesian network
 def generate_bayes():
 	bayes = dict()
 	bayes['C'] = Node("cloudy",0.5)
@@ -21,6 +24,7 @@ def generate_bayes():
 	bayes['W'] = Node("wet grass",{'sr':0.99,'s~r':0.9,'~sr':0.9,'~s~r':0.0})
 	return bayes
 
+#nodes of the bayesian network
 class Node:
 	def __init__(self,name,prob):
 		self.name = name
@@ -28,18 +32,22 @@ class Node:
 
 class Samples:
 	def __init__(self,numbers,bayes,v,s):
+		#list of numbers from the input file
 		self.numbers = numbers
+		#dictionary of nodes for bayesian network
 		self.bayes = bayes
-		self.samples = [s][v]
+		#2D array of generated samples
+		self.samples = []
+		#number of samples
 		self.numS = s
+		#number of variables in the bayesian network
 		self.numV = v
 	
-	def generate_samples(self):
-		#determine if cloudy is true or false
-		#based on that, determine sprinklers and rain (independent)
-		#based on sprinklers and rain, determine wet grass
-		#four numbers per sample, so 25 samples
-		
+	'''this function generates a 2D array of samples
+		determines whether cloudy is true or false
+		based off of that, determines rain and sprinklers
+		based off of those values, determines wet grass'''
+	def generate_samples(self):		
 		for i in range(0,self.numS-1):
 			#getting random numbers from list
 			cloudy = self.numbers.pop()
@@ -47,65 +55,72 @@ class Samples:
 			rain = self.numbers.pop()
 			wetgrass = self.numbers.pop()
 			
+			temp = []
+			
 			#determine if cloudy is true or false
 			#cloudy true
 			if cloudy < self.bayes['C'].prob:
-				self.samples[i][0] = 1
+				temp.append(1)
 				#sprinkler true or false
 				if sprinkler < self.bayes['S'].prob['c']:
-					self.samples[i][1] = 1
+					temp.append(1)
 					sprinkler = True
 				else:
-					self.samples[i][1] = 0
+					temp.append(0)
 					sprinkler = False
 				#rain true or false
 				if rain < self.bayes['R'].prob['c']:
-					self.samples[i][2] = 1
+					temp.append(1)
 					rain = True
 				else:
-					self.samples[i][2] = 0
+					temp.append(0)
 					rain = False
 			#cloudy false
 			else:
-				self.samples[i][0] = 0
+				temp.append(0)
 				#sprinkler true or false
 				if sprinkler < self.bayes['S'].prob['~c']:
-					self.samples[i][1] = 1
+					temp.append(1)
 					sprinkler = True
 				else:
-					self.samples[i][1] = 0
+					temp.append(0)
 					sprinkler = False
 				#rain true or false
 				if rain < self.bayes['R'].prob['~c']:
-					self.samples[i][2] = 1
+					temp.append(1)
 					rain = True
 				else:
-					self.samples[i][2] = 0
+					temp.append(0)
 					rain = False
 			
 			#determining wet grass based on sprinkler and rain
 			if sprinkler:
 				if rain:
 					if wetgrass < self.bayes['W'].prob['sr']:
-						self.samples[i][3] = 1
+						temp.append(1)
 					else:
-						self.samples[i][3] = 0
+						temp.append(0)
 				else:
 					if wetgrass < self.bayes['W'].prob['s~r']:
-						self.samples[i][3] = 1
+						temp.append(1)
 					else:
-						self.samples[i][3] = 0
+						temp.append(0)
 			else:
 				if rain:
 					if wetgrass < self.bayes['W'].prob['~sr']:
-						self.samples[i][3] = 1
+						temp.append(1)
 					else:
-						self.samples[i][3] = 0
+						temp.append(0)
 				else:
 					if wetgrass < self.bayes['W'].prob['~s~r']:
-						self.samples[i][3] = 1
+						temp.append(1)
 					else:
-						self.samples[i][3] = 0
+						temp.append(0)
+			self.samples.append(temp)			
+		print self.samples
+		
+	def prior_c(self,rain):
+		#rain is true or false
 				
 
 def main():
@@ -118,6 +133,7 @@ def main():
 	
 	#four variables, 25 samples (4 random numbers per sample, 100 random numbers, 25 samples)
 	s = Samples(numbers,bayes,4,25)
+	s.generate_samples()
 	
 
 if __name__ == "__main__":
